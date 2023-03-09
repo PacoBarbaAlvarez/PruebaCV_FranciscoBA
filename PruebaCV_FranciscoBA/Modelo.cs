292cf3e3f4a1fb7a9ad3b5b0e373deab6f22133f
PacoBarbaAlvarez <pacobarbitaalvarez@gmail.com>
2023-03-09 11:10:30 +0100

    Creacion de la Clase Modelo

---
+++ PruebaCV_FranciscoBA/Modelo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCV_FranciscoBA
+{
+    internal class Modelo
+    {
+    }
+}
